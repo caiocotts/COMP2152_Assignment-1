27dba1b21d0a1053b4962319f8b97410b7829526
Caio Cotts <caio@cotts.com.br>
2026-02-27 20:55:14 -0500

    handle input with difference casing

--- assignment1_studentID.py
+++ assignment1_studentID.py
@@ -14,10 +14,10 @@ membership_active = True
 # Step c: Create a dictionary named workout_stats
 workout_stats = {
     #        yoga, running, weightlifting
-    'jade': (10, 32, 23),
-    'ash': (30, 43, 53),
-    'cody': (43, 17, 30),
-    'caleb': (54, 30, 43)
+    'Jade': (10, 32, 23),
+    'Ash': (30, 43, 53),
+    'Cody': (43, 17, 30),
+    'Caleb': (54, 30, 43)
 }
 
 # Step d: Calculate total workout minutes using a loop and add to dictionary
@@ -37,7 +37,7 @@ print('weightlifting times for the last two friends:')
 [print(f'Great job staying active, {times[0]}!') for times in workout_list if sum(times[1:]) > 120]
 
 # Step h: User input to look up a friend
-inp = input("enter a friend's name to see their workout stats: ").lower()
+inp = input("enter a friend's name to see their workout stats: ").lower().capitalize()
 if inp not in workout_stats:
     print(f'friend "{inp}" not found in the records')
 else:
